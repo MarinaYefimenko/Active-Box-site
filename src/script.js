b15c644d6ee1e5e041400f56e9e7025a78858d5a
MYefimenko <marinka0410@gmail.com>
2022-10-21 21:54:17 +0200

    addedScript

--- src/script.js
+++ src/script.js
@@ -1,7 +1,42 @@
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 document.addEventListener('DOMContentLoaded', function () {
-    // showUpBtn======================================================
-    (function (btnSelector) {
+    function showUpBtn(btnSelector) {
         var upBtn = document.querySelector(btnSelector), flag = true;
         if (upBtn === null) {
             throw new Error('Could not find button');
@@ -34,10 +69,41 @@ document.addEventListener('DOMContentLoaded', function () {
             window.scrollTo(pageXOffset, 0);
             hideUpBtn();
         });
-    })('.up__btn');
-    // Modal Window ==================================================
-    (function (modalSelector, btnSelector, closeSelector) {
-        var modal = document.querySelector(modalSelector), btn = document.querySelector(btnSelector), close = document.querySelector(closeSelector);
+    }
+    ;
+    showUpBtn('.up__btn');
+    function showModal(modalSelector, modal, header) {
+        if (modal === null) {
+            throw new Error('Could not find modal window');
+        }
+        if (header === null) {
+            throw new Error('Could not find header');
+        }
+        header.style.width = document.body.offsetWidth + 'px';
+        var paddingOffset = document.body.clientWidth - document.body.offsetWidth + 'px';
+        if (paddingOffset !== '0px') {
+            document.body.style.paddingRight = paddingOffset;
+        }
+        else {
+            document.body.style.paddingRight = '17px';
+        }
+        modal.classList.add(modalSelector.substring(1) + '__show');
+        document.body.style.overflow = 'hidden';
+    }
+    function hideModal(modalSelector, modal, header) {
+        if (modal === null) {
+            throw new Error('Could not find modal window');
+        }
+        if (header === null) {
+            throw new Error('Could not find header');
+        }
+        header.style.width = '100%';
+        modal.classList.remove(modalSelector.substring(1) + '__show');
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '0';
+    }
+    function toggleModal(modalSelector, btnSelector, closeSelector, headerSelector) {
+        var modal = document.querySelector(modalSelector), btn = document.querySelector(btnSelector), close = document.querySelector(closeSelector), header = document.querySelector(headerSelector);
         if (modal === null) {
             throw new Error('Could not find modal window');
         }
@@ -47,31 +113,155 @@ document.addEventListener('DOMContentLoaded', function () {
         if (close === null) {
             throw new Error('Could not find close element');
         }
-        function showModal() {
-            if (modal === null) {
-                throw new Error('Could not find modal window');
-            }
-            modal.classList.add(modalSelector.substring(1) + '__show');
-            document.body.style.overflow = 'hidden';
-        }
-        function hideModal() {
-            if (modal === null) {
-                throw new Error('Could not find modal window');
-            }
-            modal.classList.remove(modalSelector.substring(1) + '__show');
-            document.body.style.overflow = '';
-        }
-        btn.addEventListener('click', showModal);
-        close.addEventListener('click', hideModal);
+        btn.addEventListener('click', function () { showModal(modalSelector, modal, header); });
+        close.addEventListener('click', function () { hideModal(modalSelector, modal, header); });
         modal.addEventListener('click', function (e) {
             if (e.target === modal) {
-                hideModal();
+                hideModal(modalSelector, modal, header);
             }
         });
         document.addEventListener('keydown', function (e) {
             if (e.code === 'Escape') {
-                hideModal();
+                hideModal(modalSelector, modal, header);
             }
         });
-    })('.modal', '.btn__more', '.modal__close');
+    }
+    ;
+    toggleModal('.modal', '.btn__more', '.modal__close', '.header');
+    function openBurgerMenu(burgerSelector, menuSelector, crossSelector, linkSelector) {
+        var burger = document.querySelector(burgerSelector), menu = document.querySelector(menuSelector), cross = document.querySelector(crossSelector), links = document.querySelectorAll(linkSelector);
+        if (burger === null) {
+            throw new Error('Could not find burger');
+        }
+        if (menu === null) {
+            throw new Error('Could not find menu');
+        }
+        if (cross === null) {
+            throw new Error('Could not find close element');
+        }
+        burger.addEventListener('click', function () {
+            menu.style.display = "flex";
+            burger.style.display = "none";
+            cross.style.display = "block";
+            document.body.style.overflow = 'hidden';
+        });
+        function close() {
+            if (burger === null) {
+                throw new Error('Could not find burger');
+            }
+            if (menu === null) {
+                throw new Error('Could not find menu');
+            }
+            if (cross === null) {
+                throw new Error('Could not find close element');
+            }
+            menu.style.display = "none";
+            burger.style.display = "block";
+            cross.style.display = "none";
+            document.body.style.overflow = '';
+        }
+        menu.addEventListener('click', function (e) {
+            links.forEach(function (link) {
+                if (e.target == link && document.documentElement.scrollWidth < 1000) {
+                    close();
+                }
+            });
+        });
+        cross.addEventListener('click', function () {
+            close();
+        });
+        document.addEventListener('keydown', function (e) {
+            if (e.code === 'Escape' && window.getComputedStyle(menu).display === 'flex') {
+                close();
+            }
+        });
+    }
+    ;
+    openBurgerMenu('.burger', '.nav', '.cross', '.nav__link');
+    function navigation(linksSelector, pointsSelector) {
+        var links = document.querySelectorAll(linksSelector), points = document.querySelectorAll(pointsSelector);
+        function scrollTo(point) {
+            window.scroll({
+                left: 0,
+                top: point.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+        links.forEach(function (link, i) {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+                scrollTo(points[i]);
+            });
+        });
+    }
+    ;
+    navigation('.nav__link', '.point');
+    function postRequest(formSelector, modalSelector, modalContentSelector, headerSelector) {
+        var _this = this;
+        var form = document.querySelector(formSelector);
+        if (form === null) {
+            throw new Error('Could not find form');
+        }
+        var message = {
+            success: "Thank you!<br>Please, wait for our call!",
+            failure: 'Something went wrong.<br>Please try again later.'
+        };
+        var postData = function (url, data) { return __awaiter(_this, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch(url, {
+                            method: "POST",
+                            headers: {
+                                'Content-type': 'application/json'
+                            },
+                            body: data
+                        })];
+                    case 1:
+                        res = _a.sent();
+                        return [4 /*yield*/, res];
+                    case 2: return [2 /*return*/, _a.sent()];
+                }
+            });
+        }); };
+        function bindpostData(form) {
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                var formData = new FormData(form);
+                var json = JSON.stringify(Object.fromEntries(formData.entries()));
+                postData('server.php', json)
+                    .then(function (data) {
+                    console.log(data);
+                    showThanksModal(message.success);
+                })["catch"](function () {
+                    showThanksModal(message.failure);
+                })["finally"](function () {
+                    form.reset();
+                });
+            });
+        }
+        function showThanksModal(message) {
+            var modal = document.querySelector(modalSelector), pervModalDialog = document.querySelector(modalContentSelector), header = document.querySelector(headerSelector);
+            if (modal === null) {
+                throw new Error('Could not find modal window');
+            }
+            if (pervModalDialog === null) {
+                throw new Error('Could not find modal dialog');
+            }
+            pervModalDialog.style.display = 'none';
+            showModal(modalSelector, modal, header);
+            var thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = "<div class=\"modal__content\">\n                <div class=\"modal__title\">".concat(message, "</div>\n                </div>");
+            modal.append(thanksModal);
+            setTimeout(function () {
+                thanksModal.remove();
+                pervModalDialog.style.display = 'block';
+                hideModal(modalSelector, modal, header);
+            }, 3000);
+        }
+        bindpostData(form);
+    }
+    ;
+    postRequest('.callback', '.modal', '.modal__content', '#header');
 });
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,9 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // showUpBtn======================================================
-    ((btnSelector: string): void => {
-        let upBtn: Element | null = document.querySelector(btnSelector),
+
+    function showUpBtn(btnSelector: string): void {
+        let upBtn: HTMLElement | null = document.querySelector(btnSelector),
             flag: boolean = true;
 
         if (upBtn === null) {
@@ -42,13 +42,48 @@ document.addEventListener('DOMContentLoaded', () => {
             hideUpBtn();
         })
 
-    })('.up__btn');
+    };
+    showUpBtn('.up__btn');
 
-    // Modal Window ==================================================
-    ((modalSelector: string, btnSelector: string, closeSelector: string): void => {
-        const modal: Element | null = document.querySelector(modalSelector),
-            btn: Element | null = document.querySelector(btnSelector),
-            close: Element | null = document.querySelector(closeSelector);
+    function showModal(modalSelector: string, modal: HTMLElement, header: HTMLElement | null): void {
+        if (modal === null) {
+            throw new Error('Could not find modal window')
+        }
+        if (header === null) {
+            throw new Error('Could not find header')
+        }
+
+        header.style.width = document.body.offsetWidth + 'px';
+        let paddingOffset: string = document.body.clientWidth - document.body.offsetWidth + 'px';
+        if (paddingOffset !== '0px') {
+            document.body.style.paddingRight = paddingOffset;
+        } else {
+            document.body.style.paddingRight = '17px';
+        }
+
+        modal.classList.add(modalSelector.substring(1) + '__show');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function hideModal(modalSelector: string, modal: HTMLElement, header: HTMLElement | null): void {
+        if (modal === null) {
+            throw new Error('Could not find modal window')
+        }
+        if (header === null) {
+            throw new Error('Could not find header')
+        }
+
+        header.style.width = '100%';
+        modal.classList.remove(modalSelector.substring(1) + '__show');
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '0';
+    }
+
+    function toggleModal(modalSelector: string, btnSelector: string, closeSelector: string, headerSelector: string): void {
+        const modal: HTMLElement | null = document.querySelector(modalSelector),
+            btn: HTMLElement | null = document.querySelector(btnSelector),
+            close: HTMLElement | null = document.querySelector(closeSelector),
+            header: HTMLElement | null = document.querySelector(headerSelector);
 
         if (modal === null) {
             throw new Error('Could not find modal window')
@@ -60,38 +95,178 @@ document.addEventListener('DOMContentLoaded', () => {
             throw new Error('Could not find close element')
         }
 
-        function showModal() {
-            if (modal === null) {
-                throw new Error('Could not find modal window')
-            }
-            modal.classList.add(modalSelector.substring(1) + '__show');
-            document.body.style.overflow = 'hidden';
-        }
 
-        function hideModal() {
-            if (modal === null) {
-                throw new Error('Could not find modal window')
-            }
-            modal.classList.remove(modalSelector.substring(1) + '__show');
-            document.body.style.overflow = '';
-        }
+        btn.addEventListener('click', () => { showModal(modalSelector, modal, header) });
 
-        btn.addEventListener('click', showModal);
-
-        close.addEventListener('click', hideModal);
+        close.addEventListener('click', () => { hideModal(modalSelector, modal, header) });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                hideModal();
+                hideModal(modalSelector, modal, header);
             }
         });
 
         document.addEventListener('keydown', (e) => {
             if (e.code === 'Escape') {
-                hideModal();
+                hideModal(modalSelector, modal, header);
             }
         });
-    })('.modal', '.btn__more', '.modal__close');
+    };
+    toggleModal('.modal', '.btn__more', '.modal__close', '.header');
 
+    function openBurgerMenu(burgerSelector: string, menuSelector: string, crossSelector: string, linkSelector: string): void {
+        const burger: HTMLElement | null = document.querySelector(burgerSelector),
+            menu: HTMLElement | null = document.querySelector(menuSelector),
+            cross: HTMLElement | null = document.querySelector(crossSelector),
+            links: NodeListOf<HTMLElement> = document.querySelectorAll(linkSelector);
+
+        if (burger === null) {
+            throw new Error('Could not find burger')
+        }
+        if (menu === null) {
+            throw new Error('Could not find menu')
+        }
+        if (cross === null) {
+            throw new Error('Could not find close element')
+        }
+        burger.addEventListener('click', (): void => {
+            menu.style.display = "flex";
+            burger.style.display = "none";
+            cross.style.display = "block";
+            document.body.style.overflow = 'hidden';
+        });
+
+        function close(): void {
+            if (burger === null) {
+                throw new Error('Could not find burger')
+            }
+            if (menu === null) {
+                throw new Error('Could not find menu')
+            }
+            if (cross === null) {
+                throw new Error('Could not find close element')
+            }
+            menu.style.display = "none";
+            burger.style.display = "block";
+            cross.style.display = "none";
+            document.body.style.overflow = '';
+        }
+
+        menu.addEventListener('click', (e) => {
+            links.forEach((link: HTMLElement) => {
+                if (e.target == link && document.documentElement.scrollWidth < 1000) {
+                    close();
+                }
+            })
+        });
+
+        cross.addEventListener('click', () => {
+            close();
+        })
+
+        document.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape' && window.getComputedStyle(menu).display === 'flex') {
+                close();
+            }
+        });
+    };
+    openBurgerMenu('.burger', '.nav', '.cross', '.nav__link');
+
+    function navigation(linksSelector: string, pointsSelector: string): void {
+        const links: NodeListOf<HTMLElement> = document.querySelectorAll(linksSelector),
+            points: NodeListOf<HTMLElement> = document.querySelectorAll(pointsSelector);
+
+        function scrollTo(point: HTMLElement): void {
+            window.scroll({
+                left: 0,
+                top: point.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+
+        links.forEach((link: HTMLElement, i: number) => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                scrollTo(points[i]);
+            })
+        });
+
+    };
+    navigation('.nav__link', '.point');
+
+    function postRequest(formSelector: string, modalSelector: string, modalContentSelector: string, headerSelector: string): void {
+
+        const form: HTMLFormElement | null = document.querySelector(formSelector);
+        if (form === null) {
+            throw new Error('Could not find form')
+        }
+
+        const message: { success: string, failure: string } = {
+            success: "Thank you!<br>Please, wait for our call!",
+            failure: 'Something went wrong.<br>Please try again later.',
+        };
+
+        const postData = async (url: string, data: string): Promise<Response> => {
+            const res: Response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: data
+            });
+            return await res;
+        };
+
+        function bindpostData(form: HTMLFormElement): void {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const formData = new FormData(form);
+                const json: string = JSON.stringify(Object.fromEntries(formData.entries()));
+
+                postData('server.php', json)
+                    .then(data => {
+                        console.log(data);
+                        showThanksModal(message.success);
+                    }).catch(() => {
+                        showThanksModal(message.failure);
+                    }).finally(() => {
+                        form.reset();
+                    })
+            });
+        }
+
+        function showThanksModal(message: string) {
+            const modal: HTMLElement | null = document.querySelector(modalSelector),
+                pervModalDialog: HTMLElement | null = document.querySelector(modalContentSelector),
+                header: HTMLElement | null = document.querySelector(headerSelector);
+
+            if (modal === null) {
+                throw new Error('Could not find modal window')
+            }
+            if (pervModalDialog === null) {
+                throw new Error('Could not find modal dialog')
+            }
+            pervModalDialog.style.display = 'none';
+
+            showModal(modalSelector, modal, header);
+
+            const thanksModal: HTMLDivElement = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `<div class="modal__content">
+                <div class="modal__title">${message}</div>
+                </div>`;
+
+            modal.append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                pervModalDialog.style.display = 'block';
+                hideModal(modalSelector, modal, header);
+            }, 3000);
+        }
+
+        bindpostData(form);
+    };
+    postRequest('.callback', '.modal', '.modal__content', '#header');
 
 });
